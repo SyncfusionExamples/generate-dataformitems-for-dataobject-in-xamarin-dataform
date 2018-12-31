65277963c8f91b515eb41e8baf4de2707bb46dbc
AjitAk <ajithkumar.senthilkumar@syncfusion.com>
2018-12-31 10:35:01 +0530

    Added GenerateDataFormItemsForDataObject sample in this repository

---
+++ Xamarin.Forms/GenerateDataFormItemsForDataObject/GenerateDataFormItemsForDataObject/ContactInfo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenerateDataFormItemsForDataObject
+{
+    public class ContactsInfo
+    {
+
+        private double id = 20;
+        private string _name = null;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                this.RaisePropertyChanged("Name");
+            }
+        }
+        public double ID
+        {
+            get { return id; }
+            set
+            {
+                id = value;
+                this.RaisePropertyChanged("ID");
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(String Name)
+        {
+            if (PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(Name));
+        }
+    }
+
+}
---
+++ Xamarin.Forms/GenerateDataFormItemsForDataObject/GenerateDataFormItemsForDataObject/DataFormBehavior.cs
@@ -0,0 +1,64 @@
+﻿using Syncfusion.XForms.DataForm;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace GenerateDataFormItemsForDataObject
+{
+   public class DataFormBehavior : Behavior<ContentPage>
+    {
+        SfDataForm dataForm;
+        protected override void OnAttachedTo(ContentPage bindable)
+        {
+            base.OnAttachedTo(bindable);
+
+            dataForm = bindable.FindByName<SfDataForm>("dataForm");
+            dataForm.DataObject = new ContactsInfo();
+             dataForm.ItemManager = new DataFormItemManagerExt(dataForm);
+        }
+    }
+
+    public class DataFormItemManagerExt : DataFormItemManager
+    {
+        SfDataForm dataform = new SfDataForm();
+        ContactsInfo a = new ContactsInfo();
+        public DataFormItemManagerExt(SfDataForm dataForm) : base(dataForm)
+        {
+            dataform = dataForm;
+        }
+        protected override List<DataFormItemBase> GenerateDataFormItems(PropertyInfoCollection itemProperties, List<DataFormItemBase> dataFormItems)
+        {
+            var items = new List<DataFormItemBase>();
+            foreach (var propertyInfo in itemProperties)
+            {
+                DataFormItem dataFormItem;
+                if (propertyInfo.Key == "ID")
+                    dataFormItem = new DataFormNumericItem() { Name = propertyInfo.Key, Editor = "Numeric", MaximumNumberDecimalDigits = 0 };
+                else if (propertyInfo.Key == "Name")
+                    dataFormItem = new DataFormTextItem() { Name = propertyInfo.Key, Editor = "Text" };
+                else
+                    dataFormItem = new DataFormTextItem() { Name = propertyInfo.Key, Editor = "Text" };
+
+                items.Add(dataFormItem);
+            }
+
+            return items;
+        }
+
+        public override object GetValue(DataFormItem dataFormItem)
+        {
+            var value = dataform.DataObject.GetType().GetRuntimeProperty(dataFormItem.Name).GetValue(dataform.DataObject);
+            return value;
+        }
+
+        public override void SetValue(DataFormItem dataFormItem, object value)
+        {
+            dataform.DataObject.GetType().GetRuntimeProperty(dataFormItem.Name).SetValue(dataform.DataObject, value);
+        }
+    }
+}
